1a1c680d6040ee5599836378c6b7fe1615354cb6
Priya Gopal <114797250+priyaaxx@users.noreply.github.com>
2024-04-01 21:31:42 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Finding middle element in a linked list/finding-middle-element-in-a-linked-list.java
@@ -0,0 +1,79 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+    
+}
+class GFG{
+	static void printList(Node node) 
+	{ 
+		while (node != null) 
+		{ 
+			System.out.print(node.data + " "); 
+			node = node.next; 
+		} 
+		System.out.println(); 
+	}
+    public static void main(String args[]) throws IOException { 
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t > 0){
+        	int n = sc.nextInt();
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            for(int i=0; i<n-1; i++)
+            {
+                tail.next = new Node(sc.nextInt());
+                tail = tail.next;
+            }
+            Solution g = new Solution();
+            int ans = g.getMiddle(head);
+            System.out.println(ans);
+            //printList(head); 
+            t--;
+        }
+    } 
+} 
+   
+// } Driver Code Ends
+
+
+/* Node of a linked list
+ class Node {
+   int data;
+    Node next;
+    Node(int d)  { data = d;  next = null; }
+}
+*/
+
+class Solution
+{
+    int getMiddle(Node head)
+    {
+        int size = 0;
+        Node temp = head;
+        while(temp!=null)
+        {
+            temp = temp.next;
+            size++;
+        }
+        int mid=(size/2)+1;
+        int i=1;
+        Node ans = head;
+        while(i<mid)
+        {
+            ans = ans.next;
+            i++;
+        }
+        return ans.data;
+         // Your code here.
+    }
+}
