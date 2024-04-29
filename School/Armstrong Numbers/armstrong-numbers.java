d8a326d8caad791a5f294d898349b25bc31b476f
Priya Gopal <114797250+priyaaxx@users.noreply.github.com>
2024-04-29 23:52:18 +0530

    Added Solution - GfG to GitHub

---
+++ School/Armstrong Numbers/armstrong-numbers.java
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.util.*;
+import java.io.*;
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int n = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+
+            System.out.println(ob.armstrongNumber(n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution {
+    static String armstrongNumber(int n){
+        // code here
+        int sum=0;
+        int temp=n;
+        while(temp!=0)
+        {
+            int d=temp%10;
+            int cube=d*d*d;
+            sum+=cube;
+            temp=temp/10;
+        }
+        
+        if(sum == n)
+        {
+            return "Yes";
+        }
+        else
+        {
+            return "No";
+        }
+    }
+}
