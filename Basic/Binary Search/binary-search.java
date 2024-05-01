d5c605e7936f86dff77f741727127747365ebaec
Priya Gopal <114797250+priyaaxx@users.noreply.github.com>
2024-05-01 22:24:19 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Binary Search/binary-search.java
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+  public class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0) {
+            int n = sc.nextInt();
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            int key = sc.nextInt();
+            Solution g = new Solution();
+            System.out.println(g.binarysearch(arr, n, key));
+            T--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    int binarysearch(int arr[], int n, int k) {
+        // code here
+           // code here
+           int start =0; 
+           int end = n-1;
+           while(start<=end){
+               
+               int middle = start + (end-start)/2;
+               
+              if(arr[middle]==k){
+                  return middle;
+              } else if(arr[middle]<k){
+                  start = middle + 1;
+              }else {
+                  end = middle -1;
+                  
+              }
+              
+               
+             
+           }
+           return -1;
+    }
+}
