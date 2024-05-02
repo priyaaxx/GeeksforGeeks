4ec643047b0545781894b6feea8dcdf9b4f6d425
Priya Gopal <114797250+priyaaxx@users.noreply.github.com>
2024-05-02 22:50:41 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Delete nodes having greater value on right/delete-nodes-having-greater-value-on-right.java
@@ -0,0 +1,116 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Node
+{
+
+    int data;
+    Node next;
+
+    Node(int d)
+    {
+        data = d;
+        next = null;
+    }
+}
+
+
+public class LinkedList
+{
+    
+ 
+
+    /* Function to print linked list */
+   public static void print(Node root)
+    {
+        Node temp = root;
+        while(temp!=null)
+        {
+            System.out.print(temp.data + " ");
+            temp=temp.next;
+        }
+    }
+
+
+    /* Driver program to test above functions */
+    public static void main(String args[])
+    {
+
+        /* Constructed Linked List is 1.2.3.4.5.6.
+         7.8.8.9.null */
+        int value;
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t > 0 && sc.hasNextInt() )
+        {
+            int n = sc.nextInt();
+            
+            //int n=Integer.parseInt(br.readLine());
+            int a1 = sc.nextInt();
+            Node head = new Node(a1);
+            Node tail = head;
+
+            for (int i = 1; i < n && sc.hasNextInt() ; i++)
+            {
+                int a = sc.nextInt();
+                tail.next = new Node(a);
+                tail = tail.next;
+            }
+
+
+            Node result = new Solution().compute(head);
+            print(result);
+            System.out.println();
+            t--;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*
+class Node {
+   int data;
+   Node next;
+
+  Node(int data) {
+      this.data = data;
+  }
+}
+*/
+class Solution
+{
+    Node reverse(Node head) {
+        Node next = null;
+        Node current = head;
+        Node prev = null;
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
+    }
+    Node compute(Node head)
+    {
+        // your code here
+        head = reverse(head);
+        int maxVal = head.data;
+        Node curr = head.next, prev = head;
+        Node tmp;
+        while (curr != null) {
+            // updating the value of max
+            if (curr.data >= maxVal) {
+                prev = curr;
+                maxVal = curr.data;
+            }else {
+                prev.next = curr.next;
+            }
+            curr = curr.next;
+        }
+        return reverse(head);
+        
+    }
+}
+  
